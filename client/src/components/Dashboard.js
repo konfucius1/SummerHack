84bc10ac461040eb346feebb90323f88b9c1d3ce
Thanakon Kimsan <72403268+konfucius1@users.noreply.github.com>
2023-02-08 14:36:02 +1100

    built add new playlist card and navbar

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -6,19 +6,60 @@ import logo from '../images/logo.png';
 export default function Dashboard({ code }) {
   const accessToken = useAuth(code);
   return (
-    <div>
-      <nav class="flex justify-between items-center">
-        <img src={logo} alt="" class="h-[55px] mb-4 aspect-auto" />
-        <p class="text-white font-thin">Generate</p>
-        <p class="text-white font-thin">History</p>
-        <p class="text-white font-thin">Settings</p>
-        <button class="font-montserrat btn text-white aspect-auto w-[140px] h-[60px] bg-pink-500 text-xl">
-          Log Out
-        </button>
+    <div class="bg-gray-900 h-screen">
+      <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+        <div class="container flex flex-wrap items-center justify-between mx-auto">
+          <div class="flex items-center">
+            <img src={logo} class="h-12 mr-3" alt="" />
+          </div>
+
+          <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+            <ul class="flex flex-col items-center p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <li>
+                <div class="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-pink-500 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent font-light">
+                  Generate
+                </div>
+              </li>
+              <li>
+                <div class="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-pink-500 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent font-light">
+                  History
+                </div>
+              </li>
+              <li>
+                <div class="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-pink-500 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent font-light">
+                  Settings
+                </div>
+              </li>
+              <li>
+                <button class="font-montserrat btn text-white aspect-auto w-[140px] h-[60px] bg-pink-500 text-l">
+                  Log Out
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
       </nav>
 
-      <section>
-        <h1>Your playlists</h1>
+      <section class="container p-4 m-4">
+        <h1 class="mb-4 text-xl text-white font-bold">Your playlists</h1>
+        <div class="card w-[300px] h-[400px] bg-gray-800 shadow-xl flex flex-col items-center justify-center">
+          <svg
+            class="h-10 w-10"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-20 h-20"
+          >
+            {' '}
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+            />{' '}
+          </svg>
+        </div>
       </section>
     </div>
   );
